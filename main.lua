92d35cfbcd4aaa5135aefc84e5c7d79eada68555
Aravinth Thangavel <arawindth@outlook.com>
2019-04-03 01:22:06 +0530

    added scoe, scoreFONT, moving paddles

--- main.lua
+++ main.lua
@@ -13,6 +13,13 @@ WINDOW_HEIGHT=720
 VIRTUAL_WIDTH=432
 VIRTUAL_HEIGHT=243
 
+
+player1Score = 0
+player2Score = 0
+
+player1Y = 10
+player2Y = VIRTUAL_HEIGHT-50
+
 function love.load()
 
     love.graphics.setDefaultFilter('nearest','nearest')
@@ -20,6 +27,7 @@ function love.load()
         set font
     ]]
     smallFont = love.graphics.newFont('font.ttf',8)
+    scoreFont = love.graphics.newFont('font.tff',32)
     love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT,{
@@ -35,19 +43,41 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1Y= player1Y + -player1Y*dt
+    elseif love.keyboard.isDown('s') then
+        player1Y= player1Y + player1Y*dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        player2Y= player2Y + -player2Y*dt
+    elseif love.keyboard.isDown('down') then
+        player2Y= player2Y + player2Y*dt
+    end
+end
+
 function love.draw()
 
     push:apply('start')
 
     love.graphics.clear(40,45,52,255)
 
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
     
+
+    --display score
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2-50, VIRTUAL_HEIGHT/3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/2+30, VIRTUAL_HEIGHT/3)
     --render first paddle
-    love.graphics.rectangle('fill',10,30,5,20)
+    love.graphics.rectangle('fill',10,player1Y,5,20)
 
     --render second paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-50,5,20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10,player2Y,5,20)
 
     --ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,2,2)
