8daf4c588cf6fa9001a67242d7449eeec4dc3639
Aravinth Thangavel <arawindth@outlook.com>
2019-04-03 00:53:19 +0530

    added font and paddle/ball

--- main.lua
+++ main.lua
@@ -16,6 +16,12 @@ VIRTUAL_HEIGHT=243
 function love.load()
 
     love.graphics.setDefaultFilter('nearest','nearest')
+    --[[
+        set font
+    ]]
+    smallFont = love.graphics.newFont('font.ttf',8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen =false,
         resizable = false,
@@ -32,13 +38,19 @@ end
 function love.draw()
 
     push:apply('start')
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        VIRTUAL_HEIGHT/2-6,
-        VIRTUAL_WIDTH,
-        'center'
 
-    )
+    love.graphics.clear(40,45,52,255)
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT/2-6, VIRTUAL_WIDTH, 'center')
+    
+    --render first paddle
+    love.graphics.rectangle('fill',10,30,5,20)
+
+    --render second paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-50,5,20)
+
+    --ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,2,2)
+
     push:apply('end')
 end
