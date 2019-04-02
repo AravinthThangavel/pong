29edcabbff1994a71555b336ed1a458676fac5b8
Aravinth Thangavel <arawindth@outlook.com>
2019-04-02 23:48:17 +0530

    hello wold main file

---
+++ main.lua
@@ -0,0 +1,28 @@
+--[[
+Pong Game
+]]
+
+--[[
+    Set window width and height
+]]
+WINDOW_WIDTH =1280 
+WINDOW_HEIGHT=720
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+        fullscreen =false,
+        resizable = false,
+        vsync=true   
+    })
+end
+
+function love.draw()
+    love.gaphics.printf(
+        'Hello Pong!',
+        0,
+        WINDOW_HEIGHT/2-6,
+        WINDOW_WIDTH,
+        'center'
+
+    )
+end
