73057673b1955f385a58de614b37239bb2019dbd
Aravinth Thangavel <arawindth@outlook.com>
2019-04-03 10:17:05 +0530

    added game state, ball movement

--- main.lua
+++ main.lua
@@ -14,12 +14,6 @@ VIRTUAL_WIDTH=432
 VIRTUAL_HEIGHT=243
 
 
-player1Score = 0
-player2Score = 0
-
-player1Y = 10
-player2Y = VIRTUAL_HEIGHT-50
-
 PADDLE_SPEED=200
 function love.load()
 
@@ -27,6 +21,23 @@ function love.load()
     --[[
         set font
     ]]
+    
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT-50
+
+    ballX = VIRTUAL_WIDTH/2-2
+    ballY = VIRTUAL_HEIGHT/2-2
+
+
+    ballDX =  math.random(2) ==1 and 100 or -100
+    ballDY = math.random(-50,50)
+
+    gamestate ='start'
+
+    math.randomseed(os.time())
     smallFont = love.graphics.newFont('font.ttf',8)
     scoreFont = love.graphics.newFont('font.ttf',32)
     love.graphics.setFont(smallFont)
@@ -41,22 +52,41 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return'then
+        if gamestate=='start' then
+            gamestate='play' 
+        
+        else
+            gamestate='start'
+            ballX = VIRTUAL_WIDTH/2-2
+            ballY = VIRTUAL_HEIGHT/2-2
+
+
+            ballDX = math.random(2) ==1 and 100 or -100
+            ballDY = math.random(-50,50)
+            
+
+        end
     end
 end
 
 function love.update(dt)
     -- player 1 movement
     if love.keyboard.isDown('w') then
-        player1Y= player1Y + -PADDLE_SPEED*dt
+        player1Y= math.max(0,player1Y + -PADDLE_SPEED*dt)
     elseif love.keyboard.isDown('s') then
-        player1Y= player1Y + PADDLE_SPEED*dt
+        player1Y= math.min(VIRTUAL_HEIGHT-20,player1Y + PADDLE_SPEED*dt)
     end
 
     -- player 2 movement
     if love.keyboard.isDown('up') then
-        player2Y= player2Y + -PADDLE_SPEED*dt
+        player2Y=  math.max(0,player2Y + -PADDLE_SPEED*dt)
     elseif love.keyboard.isDown('down') then
-        player2Y= player2Y + PADDLE_SPEED*dt
+        player2Y= math.min(player2Y + PADDLE_SPEED*dt)
+    end
+    if gamestate=='play' then
+        ballX= ballX + ballDX*dt
+        ballY= ballY + ballDY*dt
     end
 end
 
@@ -81,7 +111,7 @@ function love.draw()
     love.graphics.rectangle('fill', VIRTUAL_WIDTH-10,player2Y,5,20)
 
     --ball
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,2,2)
+    love.graphics.rectangle('fill', ballX,ballY,4,4)
 
     push:apply('end')
 end
